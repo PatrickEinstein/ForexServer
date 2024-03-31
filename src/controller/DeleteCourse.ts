8abd29cf6943f2014860d6b4ee81ed232c2afa96
PatrickEinstein <mohammedola1234@gmail.com>
2024-03-31 22:45:13 +0100

    courses

---
+++ src/controller/DeleteCourse.ts
@@ -0,0 +1,30 @@
+import { RequestHandler } from "express";
+import CourseModel from "../Models/Courses.js";
+import VideoModel from "../Models/Video.js";
+
+const DeleteCourse: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+
+  console.log(`id-of-course-to-delete`,id)
+
+  try {
+    const [course, videos] = await Promise.all([
+      await CourseModel.deleteOne({ _id: id }),
+
+      await VideoModel.deleteMany({ courseId: id }),
+    ]);
+
+    console.log(`courseDel==>`, course, `videoDel==>`, videos);
+   return res.status(201).json({
+        status: true,
+        message: `${course.deletedCount} courses and ${videos.deletedCount} videos were deleted` ,
+      });
+  } catch (err: any) {
+   return res.status(404).json({
+      status: false,
+      message: err.message,
+    });
+  }
+};
+
+export default DeleteCourse;
